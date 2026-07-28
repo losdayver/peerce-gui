028c210229400cf5d837fe81ab5a1aad148af44a
olegzh <losdayver@yandex.ru>
2026-07-28 22:45:24 +0300

    camelCase

--- frontend/src/components/content/fileHarbour/fileHarbour.tsx
+++ frontend/src/components/content/fileHarbour/fileHarbour.tsx
@@ -1,7 +1,7 @@
 import { FileHarbourAddPeer } from "./fileHarbourAddPeer";
 import { FileHarbourProvider } from "./fileHarbourContext";
 import { FileHarbourSidebar } from "./fileHarbourSideBar";
-import { FileHarbourWorkspace } from "./workspace/FileHarbourWorkspace";
+import { FileHarbourWorkspace } from "./workspace/fileHarbourWorkspace";
 
 export const FileHarbour: React.FC = () => {
   return (
